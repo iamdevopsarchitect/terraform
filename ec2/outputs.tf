cfcdeebde7f325ad0a7f7d18cd3f5f44ca5aa3c1
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-02-15 20:39:10 +0530

    added outputs

---
+++ ec2/outputs.tf
@@ -0,0 +1,11 @@
+# use output.instances_info
+# output.instances_info == aws_instance.expense
+output "instances_info" {
+    value = aws_instance.expense
+}
+
+output "public_ip" {
+    value       = aws_instance.terraform.public_ip
+    sensitive   = false
+    description = "Thi is the Public IP of instance created"    
+}
