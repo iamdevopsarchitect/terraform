e3f2d8cd0826872487857b1f19bd8ae645b71b16
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-03-14 12:37:18 +0530

    ec2

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform" {
 
-    ami = "ami-09c813fb71547fc4f" # left side and right side need not to be same
+    ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = {
@@ -9,24 +9,24 @@ resource "aws_instance" "terraform" {
 }
 
 resource "aws_security_group" "allow_ssh_terraform" {
-    name        = "allow_sshh" # allow_ssh is already there in my account
+    name        = "allow_sshh" #allow_ssh is already there in my account
     description = "Allow port number 22 for SSH access"
 
     # usually we allow everything in egress
     egress {
-        from_port           = 0
-        to_port             = 0
-        protocol            = "-1"
-        cidr_blocks         = ["0.0.0.0/0"]
-        ipv6_cidr_blocks    = ["::/0"]  
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
     }
 
     ingress {
-        from_port           = 22
-        to_port             = 22
-        protocol            = "tcp"
-        cidr_blocks         = ["0.0.0.0/0"] # allow from evreyone
-        ipv6_cidr_blocks    = ["::/0"]
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+        ipv6_cidr_blocks = ["::/0"]
     }
 
     tags = {
--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,8 +1,8 @@
 # use output.instances_info
 # output.instances_info == aws_instance.expense
-output "instances_info" {
-    value = aws_instance.expense
-}
+# output "instances_info" {
+#     value = aws_instance.expense
+# }
 
 output "public_ip" {
     value       = aws_instance.terraform.public_ip
