af7d6f77564d6bf21f1fb10a2c336cf9d8fa6550
kranthidevopsarchitect <iamdevopsarchitect@gmail.com>
2025-03-24 20:13:50 +0530

    jenkins

--- ec2-jen-agent/ec2.tf
+++ ec2-jen-agent/ec2.tf
@@ -40,9 +40,9 @@ resource "aws_security_group" "allow_ssh_terraform" {
     }
 
     ingress {
-        from_port           = 22
-        to_port             = 22
-        protocol            = "tcp"
+        from_port           = 0
+        to_port             = 0
+        protocol            = "-1"
         cidr_blocks         = ["0.0.0.0/0"] # allow from evreyone
         ipv6_cidr_blocks    = ["::/0"]
     }
